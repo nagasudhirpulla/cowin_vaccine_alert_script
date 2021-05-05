8d6f7be0a0e349ca5ae0ba6b84fd12bd2d3e783a
unknown <nagasudhirpulla@gmail.com>
2021-05-05 22:34:11 +0530

    dont call api after success for 15 mins, send vaccines info also in mail

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ appConf = loadAppConfig()
 distId = appConf["districtId"]
 
 nowDate = dt.datetime.now()
-numDays = 3
+numDays = 1
 datesList = [nowDate+dt.timedelta(days=k) for k in range(numDays+1)]
 
 host = appConf["host"]
@@ -24,26 +24,28 @@ def checkSessions(reqDt):
     isMailSent = False
     sessions = getSessionsInDistrict(distId, reqDt)
     if not len(sessions) == 0:
-        capacities = [t["available_capacity"] for t in sessions if t["min_age_limit"]<45]
-        if any([c>0 for c in capacities]):
+        capacityInfos = [t for t in sessions if t["min_age_limit"]<45]
+        if any([c["available_capacity"]>0 for c in capacityInfos]):
             reqDtStr = dt.datetime.strftime(reqDt, "%d-%b-%Y")
-            msg = "Please check Cowin portal <a href=https://www.cowin.gov.in/home>https://www.cowin.gov.in/home</a> for vaccine booking on {0} for age less than 45 years".format(reqDtStr)
-            if dt.datetime.now() - mailSentAt > dt.timedelta(minutes=30):
-                # dont send mail till half an hour
-                sendMail(host, port, mailUsername, mailPass, msg, fromMail, toMail, sub)
-                isMailSent = True
+            msg = "Please check Cowin portal <a href=https://www.cowin.gov.in/home>https://www.cowin.gov.in/home</a> for vaccine booking on {0} for age less than 45 years <br> ".format(reqDtStr)
+            capacityStrs = ["Available capacity = {0}, {1}, {2}".format(c["available_capacity"], c["name"], c["address"]) for c in capacityInfos]
+            msg += '<br>'.join(capacityStrs) 
+            sendMail(host, port, mailUsername, mailPass, msg, fromMail, toMail, sub)
+            isMailSent = True
     return isMailSent
 
 while True:
-    print("{0}: starting to check for vaccine sessions for next {1} days".format(dt.datetime.now(), numDays))
-    isMailSent = False
-    try:
-        for d in datesList:
-            mailSentResult = checkSessions(d)
-            if mailSentResult == True:
-                isMailSent = True
-        if isMailSent:
-            mailSentAt = dt.datetime.now()
-    except:
-        print("error occurred")
-    time.sleep(10)
+    if dt.datetime.now() - mailSentAt > dt.timedelta(minutes=15):
+        # dont check again till 15 mins after mail is sent
+        isMailSent = False
+        print("{0}: starting to check for vaccine slots for next {1} days".format(dt.datetime.now(), numDays))
+        try:
+            for d in datesList:
+                mailSentResult = checkSessions(d)
+                if mailSentResult == True:
+                    isMailSent = True
+            if isMailSent:
+                mailSentAt = dt.datetime.now()
+        except:
+            print("error occurred")
+        time.sleep(10)
