efa887b749f4a7f55875df7c6d6d9374164fb030
unknown <nagasudhirpulla@gmail.com>
2021-05-05 19:11:33 +0530

    mail silent for 30 mins

--- index.py
+++ index.py
@@ -13,16 +13,20 @@ mailUsername = appConf["mailUsername"]
 mailPass = appConf["mailPass"]
 fromMail = appConf["fromMail"]
 toMail = appConf["toMail"]
-msg = "Please check Cowin portal https://www.cowin.gov.in/home for vaccine booking for age less than 45 years"
+msg = "Please check Cowin portal <a href=https://www.cowin.gov.in/home>https://www.cowin.gov.in/home</a> for vaccine booking for age less than 45 years"
 sub = "Cowin portal Vaccine Availability alert"
 
+mailSentAt = dt.datetime.now()-dt.timedelta(minutes=35)
 def checkSessions():
     sessions = getSessionsInDistrict(distId, tomDate)
     if not len(sessions) == 0:
         capacities = [t["available_capacity"] for t in sessions if t["min_age_limit"]<45]
         if any([c>0 for c in capacities]):
-            sendMail(host, port, mailUsername, mailPass, msg, fromMail, toMail, sub)
+            if dt.datetime.now() - mailSentAt > dt.timedelta(minutes=30):
+                # dont send mail till half an hour
+                sendMail(host, port, mailUsername, mailPass, msg, fromMail, toMail, sub)
 
 while True:
+    # starting to check for vaccine sessions tomorrow 
     checkSessions()
     time.sleep(10)
