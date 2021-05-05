b216650ffeb2cf5efee8f95cb9ba021de08134ea
unknown <nagasudhirpulla@gmail.com>
2021-05-05 19:48:56 +0530

    minor changes for mail spacing

--- index.py
+++ index.py
@@ -6,27 +6,41 @@ import time
 
 appConf = loadAppConfig()
 distId = appConf["districtId"]
-tomDate = dt.datetime.now()+dt.timedelta(days=1)
+
+nowDate = dt.datetime.now()
+numDays = 3
+datesList = [nowDate+dt.timedelta(days=k) for k in range(numDays+1)]
+
 host = appConf["host"]
 port = appConf["port"]
 mailUsername = appConf["mailUsername"]
 mailPass = appConf["mailPass"]
 fromMail = appConf["fromMail"]
 toMail = appConf["toMail"]
-msg = "Please check Cowin portal <a href=https://www.cowin.gov.in/home>https://www.cowin.gov.in/home</a> for vaccine booking for age less than 45 years"
 sub = "Cowin portal Vaccine Availability alert"
 
 mailSentAt = dt.datetime.now()-dt.timedelta(minutes=35)
-def checkSessions():
-    sessions = getSessionsInDistrict(distId, tomDate)
+def checkSessions(reqDt):
+    isMailSent = False
+    sessions = getSessionsInDistrict(distId, reqDt)
     if not len(sessions) == 0:
         capacities = [t["available_capacity"] for t in sessions if t["min_age_limit"]<45]
         if any([c>0 for c in capacities]):
+            reqDtStr = dt.datetime.strftime(reqDt, "%d-%b-%Y")
+            msg = "Please check Cowin portal <a href=https://www.cowin.gov.in/home>https://www.cowin.gov.in/home</a> for vaccine booking on {0} for age less than 45 years".format(reqDtStr)
             if dt.datetime.now() - mailSentAt > dt.timedelta(minutes=30):
                 # dont send mail till half an hour
                 sendMail(host, port, mailUsername, mailPass, msg, fromMail, toMail, sub)
+                isMailSent = True
+    return isMailSent
 
 while True:
-    print("starting to check for vaccine sessions tomorrow")
-    checkSessions()
+    print("starting to check for vaccine sessions for next {0} days".format(numDays))
+    isMailSent = False
+    for d in datesList:
+        mailSentResult = checkSessions(d)
+        if mailSentResult == True:
+            isMailSent = True
+    if isMailSent:
+        mailSentAt = dt.datetime.now()
     time.sleep(10)
